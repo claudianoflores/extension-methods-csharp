ae8e83b18649a9bc51a39e7b5be128b2a79156bc
Claudiano-Flores <claudiano145@gmail.com>
2023-01-10 16:33:40 -0300

    Add project files.

---
+++ _ExtensionMethods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class ProcessFile {
+    public static void Main(string[] args)
+    {
+        DateTime dt = new DateTime(2023, 01, 10, 10, 10, 45);
+        Console.WriteLine(dt.ElapsedTime());
+
+        string s1 = "Good morning dear students";
+        Console.WriteLine(s1.Cut(10));
+    }
+}
